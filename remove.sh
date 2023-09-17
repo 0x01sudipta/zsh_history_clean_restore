b25c64563d7e5034b90b2487c3d01c314f0bd6b8
0x01sudipta <sudiptabiswas9279@gmail.com>
2023-09-17 14:52:18 +0530

    Update: Bug fixed

--- remove.sh
+++ remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 current_user=$(whoami)
-cp /home/$current_user/.zsh_history /home/$current_user/.zsh_history.cleaner
+cat /home/$current_user/.zsh_history >> /home/$current_user/.zsh_history.cleaner
 rm /home/$current_user/.zsh_history
 touch /home/$current_user/.zsh_history
 echo "zsh history has been cleaned!! (you are now quarentined from old history)"
